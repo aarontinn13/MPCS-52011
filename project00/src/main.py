ecc39e8b81cdbd4f2355ef1cf0f3a459b7cd827b
Aaron <tinn.3@buckeyemail.osu.edu>
2019-01-12 17:27:23 -0600

    saving

--- project00/src/main.py
+++ project00/src/main.py
@@ -1,6 +1,15 @@
 import sys
+import os
 
 #get file name
 file = sys.argv[-1]
 
 #get the path of the file
+path = os.path.dirname(file)
+
+print('path: ', path)
+
+'''
+with open(path, 'r') as r:
+    print(r.read())
+'''
