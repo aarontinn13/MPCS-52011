d2c9f6951c5dcb2ac94f95bfeb356264b833341d
Aaron <tinn.3@buckeyemail.osu.edu>
2019-01-13 15:59:29 -0600

    saving

--- project00/src/main.py
+++ project00/src/main.py
@@ -3,10 +3,10 @@ import os
 
 #get file name
 file = sys.argv[-1]
-newfile = file.partition('.')
 
 #get the path of the file
 path = os.path.abspath(file)
+newfile = os.path.basename(path).partition('.')
 
 with open(path, 'r') as r:
     with open('{}.out'.format(newfile[0]),'w') as w:
